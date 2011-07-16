b217e7017813486b8bd67f284dcc8404b320e5f6
Miha Zelnik <info@spleteh.si>
2011-07-16 10:35:27 +0200

    plugin

--- wp-content/plugins/mu-plugins/DruzabneIgre.php
+++ wp-content/plugins/mu-plugins/DruzabneIgre.php
@@ -127,10 +127,14 @@ register_taxonomy(
 		)
 	);
 	
-	// Add to admin_init function
+
+################################################################################
+// DODAJANJE STOLPCEV
+################################################################################	
+	
 add_filter('manage_edit-boardgame_columns', 'add_new_boardgame_columns');
 
-function add_new_boardgame_columns($gallery_columns) {
+function add_new_boardgame_columns($boardgame_columns) {
 		$new_columns['cb'] = '<input type="checkbox" />';
 
 		$new_columns['title'] = _x('Družabna igra', 'column name');
@@ -234,6 +238,49 @@ function devpress_manage_boardgame_columns( $column, $post_id ) {
 	}
 }
 
+function fb_AddThumbColumn($cols) { 
+		$cols['thumbnail'] = __('Thumbnail'); 
+		return $cols;
+	}
+ 
+	function fb_AddThumbValue($column_name, $post_id) {
+ 
+			$width = (int) 150;
+			$height = (int) 100;
+ 
+ 
+			if ( 'thumbnail' == $column_name ) {
+				// thumbnail of WP 2.9
+				$thumbnail_id = get_post_meta( $post_id, '_thumbnail_id', true );
+				// image from gallery
+				$attachments = get_children( array('post_parent' => $post_id, 'post_type' => 'attachment', 'post_mime_type' => 'image') );
+				if ($thumbnail_id)
+					$thumb = wp_get_attachment_image( $thumbnail_id, array($width, $height), true );
+				elseif ($attachments) {
+					foreach ( $attachments as $attachment_id => $attachment ) {
+						$thumb = wp_get_attachment_image( $attachment_id, array($width, $height), true );
+					}
+				}
+					if ( isset($thumb) && $thumb ) {
+						echo $thumb;
+					} else {
+						echo __('None');
+					}
+			}
+	}
+ 
+	// for posts
+	add_filter( 'manage_posts_columns', 'fb_AddThumbColumn' );
+	add_action( 'manage_posts_custom_column', 'fb_AddThumbValue', 10, 2 );
+ 
+	// for investments
+	add_filter( 'manage_boardgame_columns', 'fb_AddThumbColumn' );
+	add_action( 'manage_boardgame_custom_column', 'fb_AddThumbValue', 10, 2 );
+
+################################################################################
+// SORTIRANJE
+################################################################################	
+	
 add_filter( 'manage_edit-boardgame_sortable_columns', 'devpress_boardgame_sortable_steviloigralcev_columns' );
 
 function devpress_boardgame_sortable_steviloigralcev_columns( $columns ) {
@@ -284,45 +331,10 @@ function devpress_boardgame_sortable_zalozniki_columns( $columns ) {
 	
 
 
-function fb_AddThumbColumn($cols) { 
-		$cols['thumbnail'] = __('Thumbnail'); 
-		return $cols;
-	}
- 
-	function fb_AddThumbValue($column_name, $post_id) {
- 
-			$width = (int) 150;
-			$height = (int) 100;
- 
- 
-			if ( 'thumbnail' == $column_name ) {
-				// thumbnail of WP 2.9
-				$thumbnail_id = get_post_meta( $post_id, '_thumbnail_id', true );
-				// image from gallery
-				$attachments = get_children( array('post_parent' => $post_id, 'post_type' => 'attachment', 'post_mime_type' => 'image') );
-				if ($thumbnail_id)
-					$thumb = wp_get_attachment_image( $thumbnail_id, array($width, $height), true );
-				elseif ($attachments) {
-					foreach ( $attachments as $attachment_id => $attachment ) {
-						$thumb = wp_get_attachment_image( $attachment_id, array($width, $height), true );
-					}
-				}
-					if ( isset($thumb) && $thumb ) {
-						echo $thumb;
-					} else {
-						echo __('None');
-					}
-			}
-	}
- 
-	// for posts
-	add_filter( 'manage_posts_columns', 'fb_AddThumbColumn' );
-	add_action( 'manage_posts_custom_column', 'fb_AddThumbValue', 10, 2 );
- 
-	// for investments
-	add_filter( 'manage_boardgame_columns', 'fb_AddThumbColumn' );
-	add_action( 'manage_boardgame_custom_column', 'fb_AddThumbValue', 10, 2 );
 
 
+
+	
+	
 
 ?>
