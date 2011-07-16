4a275199c1dc08987b1e7a79db832b3ddcbfd0cf
Miha Zelnik <info@spleteh.si>
2011-07-16 22:28:37 +0200

    dodana igra Agricola

--- wp-content/themes/constellation/functions.php
+++ wp-content/themes/constellation/functions.php
@@ -27,7 +27,7 @@ if ( function_exists('register_sidebar') ) {
 ################################################################################
 // Add theme support
 ################################################################################
-
+add_theme_support('post-thumbnails');
 add_theme_support( 'automatic-feed-links' );
 add_theme_support( 'nav-menus' );
 // add_theme_support( 'post-thumbnails', array( 'post', 'page' ) );
