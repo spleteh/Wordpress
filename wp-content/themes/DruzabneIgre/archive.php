20aba509a5bee6b78c96d575dee867dd76b13889
Miha Zelnik <info@spleteh.si>
2011-08-19 10:15:25 +0200

    arhive.php

--- wp-content/themes/DruzabneIgre/archive.php
+++ wp-content/themes/DruzabneIgre/archive.php
@@ -6,10 +6,18 @@
 	<h1 class="archive-title"><?php single_cat_title(); ?></h1>
 	<?php elseif( is_tag() ) : ?>
 	<h1 class="archive-title">Posts Tagged &ldquo;<?php single_tag_title(); ?>&rdquo;</h1>
-	<?php elseif( is_tax() && !is_tax('igralci') ) : ?>
+	<?php elseif( is_tax('avtor') ) : ?>
+	<h1 class="archive-title">Avtor: <?php single_tag_title(); ?></h1>
+	<?php elseif(is_tax('ilustrator') ) : ?>
+	<h1 class="archive-title">Ilustrator: <?php single_tag_title(); ?></h1>
+	<?php elseif( is_tax('zaloznik') ) : ?>
 	<h1 class="archive-title">Založnik: <?php single_tag_title(); ?></h1>
-	<?php elseif( is_tax('igralci') ) : ?>
+	<?php elseif(is_tax('zaloznik') ) : ?>
+	<h1 class="archive-title">Založnik: <?php single_tag_title(); ?></h1>
+	<?php elseif(is_tax('igralci') ) : ?>
 	<h1 class="archive-title">Število igralcev: <?php single_tag_title(); ?></h1>
+	<?php elseif(is_tax('kategorija') ) : ?>
+	<h1 class="archive-title">Kategorija: <?php single_tag_title(); ?></h1>
 	<?php elseif (is_day()) : ?>
 	<h1 class="archive-title">Archive for <?php the_time('F jS, Y'); ?></h1>
 	<?php elseif (is_month()) : ?>
